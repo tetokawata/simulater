9e9606bda8d480c459e07205621fd97c2380ebf6
川田恵介 <keisukekawata@iss.u-tokyo.ac.jp>
2021-12-06 18:12:02 +0900

    Fix bugs.

--- R/sim_tau.R
+++ R/sim_tau.R
@@ -14,7 +14,7 @@
 #' @export
 #'
 #' @examples
-#' sim <- sim_fix_n(number_sample = 100; number_covariate = 10; core = 1)
+#' sim <- sim_tau(number_sample = 100; number_covariate = 10; core = 1)
 #'
 #' @references No.
 #
@@ -33,11 +33,6 @@ sim_tau <- function(nunber_sample,
                     adaptive_LASSO = FALSE,
                     MCP_LASSO = FALSE,
                     core = 1){
-  require(gamlr)
-  require(furrr)
-  require(glmnet)
-  require(ranger)
-  require(ncvreg)
   require(magrittr)
   # Parameter ----
   N <- nunber_sample
@@ -49,71 +44,71 @@ sim_tau <- function(nunber_sample,
     lm(Y ~ 0 + D + x) |>
       coef() %>%
       .[1]
-  }
+    }
   est_LASSO <- function(X,D,Y){
     x <- dplyr::bind_cols(D = D,X) |> as.matrix()
-    fit <- gamlr(x = x,
-                 y = Y)
+    fit <- gamlr::gamlr(x = x,
+                        y = Y)
     coef(fit)[2]
-  }
+    }
   est_adaptive_LASSO <- function(X,D,Y){
     x <- dplyr::bind_cols(D = D,X) |> as.matrix()
     temp <- lm(Y ~ x) |> coef()
     weights <- 1/abs(temp[-1])
-    cv <- cv.glmnet(x = x,
-                    y = Y,
-                    penalty.factor = 1/weights)
-    fit <- glmnet(x = x,
-                  y = Y,
-                  penalty.factor = 1/weights,
-                  lambda = cv$lambda.min)
+    cv <- glmnet::cv.glmnet(x = x,
+                            y = Y,
+                            penalty.factor = 1/weights)
+    fit <- glmnet::glmnet(x = x,
+                          y = Y,
+                          penalty.factor = 1/weights,
+                          lambda = cv$lambda.min)
     coef(fit)[2]
-  }
+    }
   est_MCP_LASSO <- function(X,D,Y){
     x <- dplyr::bind_cols(D = D,X) |> as.matrix()
-    fit <- cv.ncvreg(X = x,
-                     y = Y)
+    fit <- ncvreg::cv.ncvreg(X = x,
+                             y = Y)
     coef(fit)[2]
-  }
+    }
   est_RL_forest <- function(X,D,Y){
     x <- X |> as.matrix()
-    fit <- ranger(y = Y,
-                  x = X)
+    fit <- ranger::ranger(y = Y,
+                          x = X)
     Y.hat <- fit$predictions
-    fit <- ranger(y = D,
-                  x = X)
+    fit <- ranger::ranger(y = D,
+                          x = X)
     D.hat <- fit$predictions
     Y.oht <- Y - Y.hat
     D.oht <- D - D.hat
     fit <- lm(Y.oht ~ 0 + D.oht)
     coef(fit)
-  }
+    }
   est_RL <- function(X,D,Y){
     x <- X |> as.matrix()
-    fit <- doubleML(x = x,
-                    d = D,
-                    y = Y)
+    fit <- gamlr::doubleML(x = x,
+                           d = D,
+                           y = Y)
     coef(fit)
-  }
+    }
   est_unpenalty_LASSO <- function(X,D,Y){
     x <- dplyr::bind_cols(D = D,X) |> as.matrix()
-    fit <- gamlr(x = x,
-                 y = Y,
-                 free = "D")
+    fit <- gamlr::gamlr(x = x,
+                        y = Y,
+                        free = "D")
     coef(fit)[2]
-  }
+    }
   est_wrong_simple <- function(X,D,Y){
     x <- X$var1
     lm(Y ~ 0 + D + x) |>
       coef() %>%
       .[1]
-  }
+    }
   est_over_parameter <- function(X,D,Y){
     x <- X |> as.matrix()
     lm(Y ~ D + x) |>
       coef() %>%
       .[2]
-  }
+    }
   # Aggregation ----
   repeat_est <- function(i){
     X <-
@@ -151,11 +146,11 @@ sim_tau <- function(nunber_sample,
                "Adaptive LASSO",
                "MCP LASSO")
     )
-  }
+    }
   # Repeat----
-  plan(multisession, workers = core)
-  future_map_dfr(1:b,
-                 repeat_est,
-                 .progress = TRUE,
-                 .options = furrr_options(seed = NULL))
-}
+  future::plan(multisession, workers = core)
+  furrr::future_map_dfr(1:b,
+                        repeat_est,
+                        .progress = TRUE,
+                        .options = furrr_options(seed = NULL))
+  }
